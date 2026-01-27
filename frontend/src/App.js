0e1c5518911655474a250159cf65e061698aebdc
damiangohrh123 <damiangohrh@gmail.com>
2026-01-27 15:26:36 +0800

    Initialize React frontend with Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
+      <h1>Videotto Clip Ranking</h1>
+
+      <button>Process Video</button>
+
+      <p>Status: Not started</p>
+
+      <ul>
+        {/* Results will appear here */}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
